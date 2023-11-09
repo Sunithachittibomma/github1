8911365250d97f3dc08be8266f4852867bfc84ed
Sunithachittibomma <sunithachittibomma@gmail.com>
2023-11-09 14:08:27 +0530

    Pushing all files

---
+++ Lession.java
@@ -0,0 +1,10 @@
+class Lession
+{
+public static void main(String[] args)
+{
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+Sytem.out.println(isJavaFun);
+System.out.println(isFishTasty);
+}
+}
